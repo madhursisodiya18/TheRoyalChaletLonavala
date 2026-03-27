0ff0f22575c9934b3c4aa7de60d1a29e44d2d794
Madhur Sisodiya <madhursisodiya32@gmail.com>
2026-03-27 12:23:01 +0530

    Deployement Changes

--- app.py
+++ app.py
@@ -3134,5 +3134,5 @@ def add_header(response):
 app.register_blueprint(admin_menu_bp, url_prefix='/admin')
 app.register_blueprint(booking_api_bp, url_prefix='/api')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
